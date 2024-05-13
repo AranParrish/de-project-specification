700bd5daee020852d004604eac3806d92f774675
kusuma-gattu <kusuma.gattu@outlook.com>
2024-05-13 14:20:56 +0100

    create s3 ingestion bucket and related permissions

---
+++ extract/terraform/s3.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "ingestion_s3" {
+  bucket_prefix = "${vars.s3_ingestion_name}-"
+
+  tags = {
+    Name        = "S3IngestionZone"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_ingestion_policy" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.s3_policy_doc.json
+}
+
+data "aws_iam_policy_document" "s3_policy_doc" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.ingestion_s3.arn,
+      "${aws_s3_bucket.ingestion_s3.arn}/*",
+    ]
+  }
+}
