11b0232c34687594785e6a8f5b83455cdf567cdd
s-lai848 <sydney848lai@gmail.com>
2024-05-15 10:44:32 +0100

    add function for reading snapshot and updates

--- extract/src/read_from_db.py
+++ extract/src/read_from_db.py
@@ -2,52 +2,30 @@ from extract.src.connection import connect_to_db
 import pg8000
 from datetime import datetime
 
-def read_history_data_from_address_tb():
-    conn = connect_to_db()
-    history_data = conn.run("""SELECT * FROM address;""")
-    return history_data
+# function that reads entire contents of table at current time
 
-def read_updates_from_address_tb():
-    conn = connect_to_db()
-    updates = conn.run("""SELECT * FROM address WHERE  now() - last_updated < interval '30 minutes';""")
-    return updates
+def read_history_data_from_any_tb(tb_name):
+    valid_tb_name = ['sales_order', 'design', 'currency', 'staff', 'counterparty', 'address', 'department', 'purchase_order', 'payment_type', 'payment', 'transaction' ]
+    if tb_name in valid_tb_name:
+        conn = connect_to_db()
+        history_data = conn.run(f"""SELECT * FROM {tb_name};""")
+        print(history_data)
+        return (history_data, conn)
+    else:
+        return f"{tb_name} is not a valid table name."
 
-def read_deletions_from_address_tb():
-    conn = connect_to_db()
-    deletions = conn.run("""CREATE OR REPLACE FUNCTION return_deleted_address()
-                        RETURNS TRIGGER 
-                        AS
-                        $$
-                        BEGIN
-                        RETURN OLD;
-                        END;
-                        $$
-                        LANGUAGE plpgsql;
-                        
-                        CREATE TRIGGER after_delete_address_trigger
-                        AFTER DELETE ON address
-                        FOR EACH ROW
-                        EXECUTE FUNCTION return_deleted_address();""")
-    return deletions
+def read_updates_from_address_tb(tb_name):
+    valid_tb_name = ['sales_order', 'design', 'currency', 'staff', 'counterparty', 'address', 'department', 'purchase_order', 'payment_type', 'payment', 'transaction' ]
+    if tb_name in valid_tb_name:
+        conn = connect_to_db()
+        updates = conn.run(f"""SELECT * FROM {tb_name} WHERE  now() - last_updated < interval '20 minutes';""")
+        return (updates, conn)
+    else:
+        return f"{tb_name} is not a valid table name."
 
+# if __name__ == "__main__":
+#     read_history_data_from_address_tb()
+#     print(read_updates_from_address_tb('staff'))
+#     print(read_history_data_from_any_tb('currenc'))
 
-if __name__ == "__main__":
-    read_history_data_from_address_tb()
-    read_updates_from_address_tb()
-    read_deletions_from_address_tb()
-
-# CREATE TABLE ad_archive(
-#                         address_id INT NOT NULL,
-#                         address_line_1 VARCHAR NOT NULL
-#                         address_line_2 VARCHAR
-#                         district VARCHAR 
-#                         city VARCHAR NOT NULL
-#                         postal_code VARCHAR NOT NULL
-#                         country VARCHAR NOT NULL
-#                         phone VARCHAR NOT NULL
-#                         created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-#                         last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-#                         deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-#                         );
-    
                         
