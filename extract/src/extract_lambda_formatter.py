eddd85066fa764ac4c09a052b467bc3f5360ff2e
AranParrish <153287399+AranParrish@users.noreply.github.com>
2024-05-15 10:18:49 +0100

    finalise format data function and tests

--- extract/src/extract_lambda_formatter.py
+++ extract/src/extract_lambda_formatter.py
@@ -5,11 +5,18 @@ def format_data(result: list, db: str) -> dict | list:
     For a single row query result, returns as a dictionary of key-value pairs
     with column headings as the keys.
     For multiple row query results, returns a list of dictionary key-value pairs.
-    Args:   result  - list representing the output from a pg8000 query of a database
-            db      - str representing the database connection used to return the query result
-                    (required to get column headings)
-    Return:     dict (for single row query result)
-                or list of dict (for multiple row query results)
+    Args:
+        result:
+            list representing the output from a pg8000 query of a database
+        db:
+            str representing the database connection used to return the query result
+            (required to get column headings)
+    
+    Returns:
+        Single dictionary:
+            For single row query result with key-value pairs using table column headings as keys
+        List of dictionaries:
+            For multiple row query results with key-value pairs in each dictionary using column headings as keys
     """
     col_headers = [col["name"] for col in db.columns]
     if len(result) == 1:
--- extract/tests/test_extract_lambda_formatter.py
+++ extract/tests/test_extract_lambda_formatter.py
@@ -2,28 +2,32 @@ from extract.src.extract_lambda_formatter import format_data
 import pytest
 # Suggest adding a separate util function for the database connection (and import for tests)
 
-# Example unit tests for data formatter taken from a previous sprint.
-
-# Amend fixture to connect to RDS test database
 @pytest.fixture(autouse=True)
 def run_db():
     db = connect_to_db()
-    seed_db()
     yield db
     db.close()
 
 @pytest.mark.describe('Format data function unit tests')
-class TestUnitTests:
+class TestFormatData:
 
-    @pytest.mark.it('Returns expected result for a single row query result')
+    @pytest.mark.it('Returns expected format and keys for a single row query result on sales order table')
     def test_format_data_single_item(self, run_db):
-        # Amend to use RDS test database
-        result = run_db.run("""SELECT * FROM treasures WHERE treasure_id = 1;""")
-        assert format_data(result, run_db) == {'treasure_id': 1, 'treasure_name': 'treasure-a', 'colour': 'turquoise', 
-                                                'age': 200, 'cost_at_auction': 20.0, 'shop_id': 1}
+        test_query_result = run_db.run("""SELECT * FROM sales_order WHERE sales_order_id = 1;""")
+        output = format_data(test_query_result, run_db)
+        expected_keys = ['sales_order_id', 'created_at', 'last_updated', 'design_id', 'staff_id', \
+                         'counterparty_id', 'units_sold', 'unit_price', 'currency_id', \
+                            'agreed_delivery_date', 'agreed_payment_date', 'agreed_delivery_location_id']
+        assert isinstance(output, dict)
+        assert all([key in output.keys() for key in expected_keys])
 
-    @pytest.mark.it('Returns expected result for a multiple row query result')    
+
+    @pytest.mark.it('Returns expected format and keys for a multiple row query result on design table')    
     def test_format_data_multiple_items(self, run_db):
-        # Amend to use RDS test database
-        result = run_db.run("""SELECT * FROM treasures WHERE treasure_id < 3;""")
-        assert format_data(result, run_db) == [{'treasure_id': 1, 'treasure_name': 'treasure-a', 'colour': 'turquoise', 'age': 200, 'cost_at_auction': 20.0, 'shop_id': 1}, {'treasure_id': 2, 'treasure_name': 'treasure-d', 'colour': 'azure', 'age': 100, 'cost_at_auction': 1001.0, 'shop_id': 2}]
+        test_query_result = run_db.run("""SELECT * FROM design LIMIT 5;""")
+        output = format_data(test_query_result, run_db)
+        expected_keys = ['design_id', 'created_at', 'last_updated', 'design_name', 'file_location', \
+                         'file_name']
+        assert isinstance(output, list)
+        for result in output:
+            assert all([key in result.keys() for key in expected_keys])
