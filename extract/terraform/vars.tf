826b6c5c4909ba89fd7d82c5a34b117e6c5502f6
kusuma-gattu <kusuma.gattu@outlook.com>
2024-05-16 23:33:56 +0100

    add variables for username etc

--- extract/terraform/vars.tf
+++ extract/terraform/vars.tf
@@ -7,3 +7,28 @@ variable "lambda_name" {
     type = string
     default = "extract-lambda"
 }
+
+variable "db_username" {
+    type = string
+    sensitive = true
+}
+
+variable "db_password" {
+    type = string
+    sensitive = true
+}
+
+variable "db_database" {
+    type = string
+    default = "totesys"
+}
+
+variable "db_host" {
+    type = string
+    default = "nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com"
+}
+
+variable "db_port" {
+    type = string
+    default = "5432"
+}
